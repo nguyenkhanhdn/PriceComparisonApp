6b9f0aa672207087b0edb16f8d923cfbe0e909dd
Nguyen Khanh <nguyenkhanhit@live.com>
2025-03-16 09:07:34 +0700

    add some change 16-03-2025

---
+++ Migrations/20250316012107_mot.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PriceComparisonApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class mot : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
